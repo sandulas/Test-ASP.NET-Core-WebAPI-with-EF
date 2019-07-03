38d9fbd4b1146661c024029125b52cd872074b2f
Adrian Sandu <adrian.sandu@pronetis.com>
2019-07-03 17:04:07 +0300

    Added initial project from template, cleaned up a bit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_ASP.NETCore_WebAPI_EF.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TodoController : ControllerBase
+	{
+		// GET api/todo
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> GetTodoList()
+		{
+			return new string[] { "todo 1", "todo 2" };
+		}
+	}
+}
