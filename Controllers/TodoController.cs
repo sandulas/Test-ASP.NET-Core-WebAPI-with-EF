d18a8f60b709f4aa2926abeabdaa8728b1a110e8
Adrian Sandu <adrian.sandu@pronetis.com>
2019-07-10 17:11:01 +0300

    Added delays to all controller actions Updated the jMeter test plan to load test both sync and async methods Some code cleanup

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -10,29 +10,38 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
+	public static class Config
+	{
+		public const int MaxWorkerThreads = 1000;
+		public const int MaxIOThreads = 1000;
+		public const int ActionDelay = 10;
+	}
+
 	// Controller for sync actions
 	[Route("api/[controller]")]
 	[ApiController]
 	public class TodoController : ControllerBase
 	{
 		private readonly TodoDbContext _dbContext;
+
 		public TodoController(TodoDbContext dbContext)
 		{
 			_dbContext = dbContext;
+
+			ThreadPool.SetMaxThreads(Config.MaxWorkerThreads, Config.MaxIOThreads);
 		}
 
 		// GET api/todo
 		[HttpGet]
 		public ActionResult<string> Welcome()
 		{
-			int maxWorkerThreads, maxPortThreads, availableWorkerThreads, availablePortThreads;
-			ThreadPool.SetMaxThreads(50, 50);
-			ThreadPool.GetMaxThreads(out maxWorkerThreads, out maxPortThreads);
-			ThreadPool.GetAvailableThreads(out availableWorkerThreads, out availablePortThreads);
+			int maxWorkerThreads, maxIOThreads, availableWorkerThreads, availableIOThreads;
+			ThreadPool.GetMaxThreads(out maxWorkerThreads, out maxIOThreads);
+			ThreadPool.GetAvailableThreads(out availableWorkerThreads, out availableIOThreads);
 
-			Task.Delay(100).Wait();
+			Task.Delay(Config.ActionDelay).Wait();
 
-			return $@"Welcome to the Todo API (worker threads: { maxWorkerThreads - availableWorkerThreads } / { maxWorkerThreads }; I/O threads: { maxPortThreads - availablePortThreads } / { maxPortThreads }; Time: { DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff") }).
+			return $@"Welcome to the Todo API (worker threads: { maxWorkerThreads - availableWorkerThreads } / { maxWorkerThreads }; I/O threads: { maxIOThreads - availableIOThreads } / { maxIOThreads }; Time: { DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff") }).
 Operations:
 - GET /todo/list (list all todo's)
 - GET /todo/id (get a todo item)
@@ -46,6 +55,8 @@ Operations:
 		[HttpGet("list")]
 		public ActionResult<IEnumerable<TodoItem>> GetTodoList()
 		{
+			Task.Delay(Config.ActionDelay).Wait();
+
 			return _dbContext.TodoItems.ToList();
 		}
 
@@ -53,6 +64,8 @@ Operations:
 		[HttpGet("{id}")]
 		public ActionResult<TodoItem> GetTodoItem(long id)
 		{
+			Task.Delay(Config.ActionDelay).Wait();
+
 			var todoItem = _dbContext.TodoItems.Find(id);
 
 			if (todoItem == null)
@@ -65,6 +78,8 @@ Operations:
 		[HttpPost]
 		public ActionResult<TodoItem> AddTodoItem(TodoItem item)
 		{
+			Task.Delay(Config.ActionDelay).Wait();
+
 			_dbContext.TodoItems.Add(item);
 			_dbContext.SaveChanges();
 
@@ -75,6 +90,8 @@ Operations:
 		[HttpPut("{id}")]
 		public ActionResult UpdateTodoItem(long id, TodoItem item)
 		{
+			Task.Delay(Config.ActionDelay).Wait();
+
 			if (id != item.Id)
 				return BadRequest();
 
@@ -88,6 +105,8 @@ Operations:
 		[HttpPatch("{id}")]
 		public ActionResult<TodoItem> PatchTodoItem(long id, JsonPatchDocument<TodoItem> patchDocument)
 		{
+			Task.Delay(Config.ActionDelay).Wait();
+
 			if (patchDocument == null)
 				return BadRequest(ModelState);
 
@@ -110,6 +129,8 @@ Operations:
 		[HttpDelete("{id}")]
 		public ActionResult DeleteTodoItem(long id)
 		{
+			Task.Delay(Config.ActionDelay).Wait();
+
 			var todoItem = _dbContext.TodoItems.Find(id);
 
 			if (todoItem == null)
@@ -131,12 +152,36 @@ Operations:
 		public TodoAsyncController(TodoDbContext dbContext)
 		{
 			_dbContext = dbContext;
+
+			ThreadPool.SetMaxThreads(Config.MaxWorkerThreads, Config.MaxIOThreads);
+		}
+
+		// GET api/todo
+		[HttpGet]
+		public async Task<ActionResult<string>> WelcomeAsync()
+		{
+			int maxWorkerThreads, maxIOThreads, availableWorkerThreads, availableIOThreads;
+			ThreadPool.GetMaxThreads(out maxWorkerThreads, out maxIOThreads);
+			ThreadPool.GetAvailableThreads(out availableWorkerThreads, out availableIOThreads);
+
+			await Task.Delay(Config.ActionDelay);
+
+			return $@"Welcome to the Todo API (worker threads: { maxWorkerThreads - availableWorkerThreads } / { maxWorkerThreads }; I/O threads: { maxIOThreads - availableIOThreads } / { maxIOThreads }; Time: { DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff") }).
+Operations:
+- GET /todo/list (list all todo's)
+- GET /todo/id (get a todo item)
+- POST /todo (add a new todo item)
+- PUT /todo/id (replace a todo item)
+- PATCH /todo/id (update a todo item)
+- DELETE /todo/id (delete a todo item)";
 		}
 
 		// GET api/todo/list
 		[HttpGet("list")]
 		public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoListAsync()
 		{
+			await Task.Delay(Config.ActionDelay);
+
 			return await _dbContext.TodoItems.ToListAsync();
 		}
 
@@ -144,6 +189,8 @@ Operations:
 		[HttpGet("{id}")]
 		public async Task<ActionResult<TodoItem>> GetTodoItemAsync(long id)
 		{
+			await Task.Delay(Config.ActionDelay);
+
 			var todoItem = await _dbContext.TodoItems.FindAsync(id);
 
 			if (todoItem == null)
@@ -156,6 +203,8 @@ Operations:
 		[HttpPost]
 		public async Task<ActionResult<TodoItem>> AddTodoItemAsync(TodoItem item)
 		{
+			await Task.Delay(Config.ActionDelay);
+
 			_dbContext.TodoItems.Add(item);
 			await _dbContext.SaveChangesAsync();
 
@@ -166,6 +215,8 @@ Operations:
 		[HttpPut("{id}")]
 		public async Task<ActionResult> UpdateTodoItemAsync(long id, TodoItem item)
 		{
+			await Task.Delay(Config.ActionDelay);
+
 			if (id != item.Id)
 				return BadRequest();
 
@@ -179,6 +230,8 @@ Operations:
 		[HttpPatch("{id}")]
 		public async Task<ActionResult<TodoItem>> PatchTodoItemAsync(long id, JsonPatchDocument<TodoItem> patchDocument)
 		{
+			await Task.Delay(Config.ActionDelay);
+
 			if (patchDocument == null)
 				return BadRequest(ModelState);
 
@@ -201,6 +254,8 @@ Operations:
 		[HttpDelete("{id}")]
 		public async Task<ActionResult> DeleteTodoItemAsync(long id)
 		{
+			await Task.Delay(Config.ActionDelay);
+
 			var todoItem = await _dbContext.TodoItems.FindAsync(id);
 
 			if (todoItem == null)
